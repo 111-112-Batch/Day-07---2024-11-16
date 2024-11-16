3fef1c2742ea09354128b0bbf026e12e4c0dd032
icet-sharadha <icet.sharadha@gmail.com>
2024-11-16 15:51:54 +0530

    API

--- app.js
+++ app.js
@@ -7,21 +7,59 @@
 // console.log(document);
 
 
-const names = ["saman", "kamal", "nimal", "sunil"];
+// const names = ["saman", "kamal", "nimal", "sunil"];
 
 
-function addName() {
-    let txtUserInput = document.getElementById("txtInput").value;
+// function addName() {
+//     let txtUserInput = document.getElementById("txtInput").value;
 
-    names.push(txtUserInput);
+//     names.push(txtUserInput);
     
-    document.getElementById("h1").innerText = names.toString();
+//     document.getElementById("h1").innerText = names.toString();
 
-    let body = "";
+//     let body = "";
 
-    for (let name of names) {
-        body += `<li>${name}</li>`;
-    }
-    document.getElementById("oList").innerHTML = body;
-}
+//     for (let name of names) {
+//         body += `<li>${name}</li>`;
+//     }
+//     document.getElementById("oList").innerHTML = body;
+// }
+
+
+// https://fakestoreapi.com/products
+let body="";
+
+fetch("https://fakestoreapi.com/products")
+.then(res=>res.json())
+.then(data=>{
+    data.forEach(element => {
+        console.log(element);
+        
+        body+= `
+        <div class="col">
+<div class="card shadow-sm">
+  <img src="${element.image}" alt="">
+  <div class="card-body">
+    <h5 class="card-text">${element.title}</h5>
+    <p class="cart-text">${element.description}</p>
+    <div class="d-flex justify-content-between align-items-center">
+      <div class="btn-group">
+        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+      </div>
+      <small class="text-body-secondary">${element.price}$</small>
+    </div>
+  </div>
+</div>
+</div> 
+        `;
+    });
+
+    document.getElementById("row").innerHTML=body;
+})
+
+
+console.log(body);
+
+
 
